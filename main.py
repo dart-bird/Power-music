e72de76b7d47718e92916065660d2a3845442492
krumaska <okokrt@gmail.com>
2021-07-10 21:08:48 +0900

    :rocket: Complete, Convert  Melon charts to yt links

--- main.py
+++ main.py
@@ -1,35 +1,74 @@
-import requests
-import json
+import requests._internal_utils
 from bs4 import BeautifulSoup
-from youtube_search import YoutubeSearch
+import datetime
+import uyts
 
-def get_html(url):
-    html = requests.get(url, headers={"User-Agent": "XY"})
-    soup = BeautifulSoup(html.text, 'html.parser')
-    return soup
-
-def get_songs_top_rank100(song_code):
-    soup = get_html('https://www.melon.com/chart/month/index.htm?classCd=' + song_code)
-    song_title_elements = soup.find_all("div", class_="ellipsis rank01")
-    song_singer_elements = soup.find_all("div", class_="ellipsis rank02")
-    song_album_elements = soup.find_all("div", class_="ellipsis rank03")
+class Melon_to_yt:
+    def __init__(self):
+        super().__init__()
+        self.code = [
+            'GN0000','DM0000','AB0000'
+            'GN0100','GN0200','GN0300','GN0400','GN0500','GN0600','GN0700','GN0800',
+            'GN0900','GN1000','GN1100','GN1200','GN1300','GN1400',
+            'GN1500','GN1700','GN1800','GN1900','GN2000','GN2100','GN2200','GN2300','GN2400',
+        ]
     
-    songs = []
-    
-    for idx in range(len(song_title_elements)-1):
-        song_data = {
-            "title" : song_title_elements[idx].find_all("a")[0].text,
-            "singer" : song_singer_elements[idx].find_all("a")[0].text,
-            "album" : song_album_elements[idx].find_all("a")[0].text
-        }
-        songs.append(song_data)
+    def __get_response(self, url):
+        return requests.get(url, headers={
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        })
 
-    return songs
+    def __get_songs_melon_chart_100(self, time, song_code):
+        url='https://www.melon.com/chart/{0}/index.htm?classCd={1}'.format(time, song_code)
+        if time not in ['day','week','month']:
+            now = datetime.datetime.now()
+            day_time = str(now.year) + "{:02d}".format(now.month) + "{:02d}".format(now.day) + "{:02d}".format(now.hour) + '00'
+            url='https://www.melon.com/chart/index.htm?dayTime='
+        response = self.__get_response(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        song_title_elements = soup.find_all("div", class_="ellipsis rank01")
+        song_singer_elements = soup.find_all("div", class_="ellipsis rank02")
+        song_album_elements = soup.find_all("div", class_="ellipsis rank03")
 
-def get_yt_link(title):
-    results = json.loads(YoutubeSearch(title, max_results=1).to_json())
-    return 'https://www.youtube.com' + results['videos'][0]['url_suffix']
+        songs = []
+
+        for idx in range(len(song_title_elements)-1):
+            song_data = {
+                "title" : song_title_elements[idx].find_all("a")[0].text,
+                "singer" : song_singer_elements[idx].find_all("a")[0].text,
+                "album" : song_album_elements[idx].find_all("a")[0].text
+            }
+            songs.append(song_data)
+        self.songs = songs
+        return songs
+    def __search_yt(self, title : str):
+        masking_word = ['1hour', '1시간', '연속재생', '플레이리스트', 'playlist', '풀영상']
+        search = uyts.Search(title)
+        # skip video data format like 1hour, playlist ... etc
+        for video in search.resultsJSON:
+            filtered = list(filter(lambda x:x in video['title'], masking_word))
+            if len(filtered) < 1:
+                return video
+            else : continue
+    def get_yt_data(self, time : str, code = 'GN0100') -> list:
+        if code in self.code:
+            self.__get_songs_melon_chart_100(time='month', song_code=code)
+            yt_data = []
+            for song in self.songs:
+                yt_data.append(self.__search_yt(song['title']))
+                print('{0} of {1} Searched Music! Title : {2}'.format(
+                    len(self.songs),
+                    len(yt_data),
+                    yt_data[len(yt_data)-1]['title']))
+            return yt_data
+        else:
+            return []
 
 if __name__ == "__main__":
-    songs = get_songs_top_rank100('GN0100') #GN0100 발라드
-    print(get_yt_link(songs[0]['title']))
+    melon = Melon_to_yt()
+    now = datetime.datetime.now()
+    day_time = str(now.year) + "{:02d}".format(now.month) + "{:02d}".format(now.day) + "{:02d}".format(now.hour) + '00'
+    video = melon.get_yt_data(time=day_time,code='GN0100')
+    video = melon.get_yt_data(time='day',code='GN0100')
+    video = melon.get_yt_data(time='month',code='GN0100')
+    video = melon.get_yt_data(time='year',code='GN0100')
